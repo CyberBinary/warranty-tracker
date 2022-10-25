e3e01543c98571e0f661900d351363c7be5b8b7c
antics710 <113466880+antics710@users.noreply.github.com>
2022-10-25 12:16:20 -0700

    restarted another branch
    Has hamburger menu  ready and has the add device page ready. Just needs the button to be able to go to the next activity (addDevice). Having trouble having the page up

--- app/src/main/java/com/example/warrantytracker/MainActivity.java
+++ app/src/main/java/com/example/warrantytracker/MainActivity.java
@@ -1,14 +1,148 @@
 package com.example.warrantytracker;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+    RecyclerView recyclerView;
+    ArrayList<String> name, email, age;
+    DBHelper DB;
+    MyAdapter adapter;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.addDeviceButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, addDevice.class);
+                startActivity(intent);
+
+            }
+        });
+
+        DB = new DBHelper(this);
+        name = new ArrayList<>();
+        email = new ArrayList<>();
+        age = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerview);
+        adapter = new MyAdapter(this, name, email, age);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        displaydata();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                    {
+                        Toast.makeText(MainActivity.this, "Home Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.contact:
+                    {
+                        Toast.makeText(MainActivity.this, "Contact Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.gallery:
+                    {
+                        Toast.makeText(MainActivity.this, "Gallery Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.login:
+                    {
+                        Toast.makeText(MainActivity.this, "LogIn Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.profilePic:
+                    {
+                        Toast.makeText(MainActivity.this, "Profile Pic Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.shareIt:
+                    {
+                        Toast.makeText(MainActivity.this, "Share It Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.settings:
+                    {
+                        Toast.makeText(MainActivity.this, "Settings Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void displaydata() {
+        Cursor cursor = DB.getdata();
+        if(cursor.getCount()==0){
+            Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            while(cursor.moveToNext()){
+                name.add(cursor.getString(0));
+                email.add(cursor.getString(1));
+                age.add(cursor.getString(2));
+
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/warrantytracker/addDevice.java
@@ -0,0 +1,56 @@
+package com.example.warrantytracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addDevice extends AppCompatActivity {
+    EditText name, email, age;
+    Button insert, view;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        age = findViewById(R.id.age);
+        insert = findViewById(R.id.btnInsert);
+        view = findViewById(R.id.btnView);
+
+        DB = new DBHelper(this);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(addDevice.this, MainActivity.class));
+            }
+        });
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String emailTXT = name.getText().toString();
+                String ageTXT = name.getText().toString();
+
+                Boolean checkinsertdata = DB.insertuserdata(nameTXT, emailTXT, ageTXT);
+                if (checkinsertdata == true){
+                    Toast.makeText(addDevice.this, "New", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(addDevice.this, "", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
+
