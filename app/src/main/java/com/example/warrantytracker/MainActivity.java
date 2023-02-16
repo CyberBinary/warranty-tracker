89fef5277256b799ddffb24876c086685285e993
CyberBinary <42885189+CyberBinary@users.noreply.github.com>
2023-02-16 12:24:59 -0800

    Swipe Left to delete
    Now you swipe left

--- app/src/main/java/com/example/warrantytracker/MainActivity.java
+++ app/src/main/java/com/example/warrantytracker/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         });
         initRecyclerView();
 
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
